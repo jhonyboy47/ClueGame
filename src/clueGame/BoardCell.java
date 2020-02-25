31554718a406042700dd23ad727f71e9ece572e4
Jhonathan Malagon <malagon@mines.edu>
2020-02-25 11:38:40 -0700

    Added Baldwin's tests and his data files

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,23 @@
+// Authors: Michael Crews and Jhonathan Malagon
+package clueGame;
+
+public class BoardCell {
+	
+	// Declare two instance variables that every boardcell has
+	int row, column;
+	
+	// This is a parameterized constructor for boardcell to pass in the row and column
+	public BoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+	
+}
