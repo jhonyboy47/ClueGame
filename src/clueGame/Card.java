8dfe40d1bd34a75018a3e07719b59338e30fe840
Michael Crews <crews@mines.edu>
2020-03-28 15:29:01 -0600

    Added classes/enum: Card, CardType, ComputerPlayer, HumanPlayer, Player

---
+++ src/clueGame/Card.java
@@ -0,0 +1,11 @@
+package clueGame;
+
+public class Card {
+	private String cardName;
+	public Boolean equals() {
+		// TODO remove null later
+		return null;
+		
+	}
+	
+}
---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,18 @@
+package clueGame;
+
+import java.util.*;
+
+public class ComputerPlayer {
+	public BoardCell pickLocation(Set<BoardCell> targets) {
+		// TODO Remove null
+		return null;
+	}
+	
+	public void makeAccusation() {
+		
+	}
+	
+	public void createSuggestion() {
+		
+	}
+}
