47e54abd28c1397f667ed4376a37755ef3dc0634
jhonyboy47 <malagon@mymail.mines.edu>
2020-03-28 17:28:40 -0500

    Failing test for loading people

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -8,6 +8,16 @@ public class Player {
 	private int column;
 	private Color color;
 	
+	
+	
+	public Player(String playerName, int row, int column, Color color) {
+		super();
+		this.playerName = playerName;
+		this.row = row;
+		this.column = column;
+		this.color = color;
+	}
+
 	public Card disproveSuggestion(Solution suggestion) {
 		return null;
 		
--- src/tests/gameSetupTests.java
+++ src/tests/gameSetupTests.java
@@ -1,5 +1,10 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Color;
+import java.lang.reflect.Field;
 import java.util.*;
 
 import org.junit.Before;
@@ -20,12 +25,43 @@ public class gameSetupTests {
 		board.setPlayersFile("ClueGamePlayers.txt");
 		board.initialize();
 	}
+	public Color convertColor(String strColor) {
+		 Color color;
+		 try {
+		 // We can use reflection to convert the string to a color
+		 Field field = Class.forName("java.awt.Color").getField(strColor.trim());
+		 color = (Color)field.get(null);
+		 } catch (Exception e) {
+		 color = null; // Not defined
+		 }
+		 return color;
+		}
+
 	
 	@Test
 	public void Test() {
 		Set<Player> players = board.getPlayersSet();
 		
+		assertTrue(players.contains(new Player("Bob", 12, 14, convertColor("blue"))));
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
