444925158612e601dc77219d2ee29a793954380d
jhonyboy47 <malagon@mymail.mines.edu>
2020-04-01 18:20:32 -0500

    Failing test's for selecting a target

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -5,13 +5,32 @@ import java.awt.Color;
 import java.util.*;
 
 public class ComputerPlayer extends Player{
+	private Set<Character> visitedSet;
+
+
 	
 	// visited list
 	public ComputerPlayer(String playerName, Integer row, Integer column, Color color) {
 		super(playerName, row, column, color);
+		visitedSet = new HashSet<Character>();
 	}
 	public BoardCell pickLocation(Set<BoardCell> targets) {
-		// TODO Remove null
+		boolean roomFound = false;
+		
+		ArrayList<BoardCell> tempTargetList = new ArrayList<BoardCell>(targets);
+		
+		System.out.println(tempTargetList);
+		
+		Collections.shuffle(tempTargetList);
+		
+		for(BoardCell boardCell: tempTargetList) {
+			
+			if (boardCell.isRoom() && !(visitedSet.contains(boardCell.getInitial()))) {
+				
+				return boardCell;
+			}
+		}
+		
 		return null;
 	}
 	
